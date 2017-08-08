3470bd13c6bad267d26e635a22c8f979a6ea4c5a
RB-Eldritch <jamesreece1496@gmail.com>
2017-08-08 13:43:57 +1000

    copy commit

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -10,7 +10,7 @@ namespace Functions {
         public GameObject projectilePrefab;
 
         //speed at which the the projectile will be flung
-        public float projectileSpeed = 500f;//from 20 hgfdeabgtrcopy
+        public float projectileSpeed = 500f;//from 20 copy MERGE PLS 1
         //Rate of Fire
         public float shootRate = 0.1f;
 
